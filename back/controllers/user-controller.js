369e384d35cf7459f487280ea08a82bea1dc7a03
khersinpro <khersinpro@gmail.com>
2022-07-14 16:48:41 +0200

    add user connexion controller/route valid (without JWT/COOKIE)

--- back/controllers/user-controller.js
+++ back/controllers/user-controller.js
@@ -1,5 +1,6 @@
 const {connect} = require('../DB.config/db.connexion');
 const bcrypt = require('bcrypt');
+const { json } = require('express');
 
 //*** Création d'un utilisateur ***/
 exports.createUser = (req, res, next) => {
@@ -13,14 +14,41 @@ exports.createUser = (req, res, next) => {
         // Connexion a la base de donnée, puis insertion des données utilsiateur grâce a sql et userData
         connect.query(sql, userData, (error, results, fields) => {
             if(error){ 
-                console.log(req.body);
                 return res.status(400).json(error)
             }
-            res.status(201).json({message: 'Votre compte à été crée avec succés.'})
+            res.status(201).json('Votre compte à été crée avec succés.')
         });
     })
     .catch(error => {
-        res.status(500).json(new Error("Une erreur est survenue, veuillez réessayer plus tard."))
+        res.status(500).json("Une erreur est survenue, veuillez réessayer plus tard.")
     });
 };
 //*** Connexion d'un utilisateur ***/
+exports.connexion = (req, res, next) => {
+    // Action a effectuer dans la base de donnée
+    const sql = "SELECT id, password FROM user WHERE email = ?";
+    try{
+        connect.query(sql, req.body.email, (error, results, fields) => {
+            // Si il y a une erreur ou aucun resultats
+            if(error){
+                return res.status(400).json(error);
+            }else if(!results[0]){
+                return res.status(404).json('E-mail ou mot de passe incorrect.');
+            };
+            // Comparaison des mot de passe
+            bcrypt.compare(req.body.password, results[0].password)
+            .then(valid => {
+                // Si le mot de passe est faux
+                if(!valid){
+                    return res.status(401).json("E-mail ou mot de passe incorrect.");
+                };
+                res.status(200).json("Connexion réussi.")  ;      
+            })
+            .catch(error => {
+                res.status(400).json("Une erreur est survenue, veuillez réessayer plus tard.");
+            });
+        });
+    }catch(error){
+        res.status(500).json(error);
+    };
+};
--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const user = require('../controllers/user-controller');
 
-router.post('/createUser', user.createUser)
+router.post('/createUser', user.createUser);
+router.post('/connexion', user.connexion);
 
 module.exports = router;
