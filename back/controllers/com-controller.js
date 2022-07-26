e42350092a7badabb02ea0f207febaf2909b12db
khersinpro <khersinpro@gmail.com>
2022-07-26 15:16:54 +0200

    untrack files contained in the .gitignore file

--- back/controllers/com-controller.js
+++ back/controllers/com-controller.js
@@ -1,24 +1,25 @@
 const {connect} = require('../DB.config/db.connexion');
 
-//*** Création d'un commentaire ***//
-//--------------------------------//
+// Création d'un commentaire 
 exports.createCom = (req, res, next) => {
     const create = 'INSERT INTO comment SET ?';
     const {post_id, comment, date, user_id} = req.body;
     const options = {post_id, comment, date, user_id};
     try{
+        if(req.body.user_id !== req.auth.user_id){
+            res.clearCookie('jwt')
+            return res.status(401).json("Unauthorized")
+        }
         connect.query(create, options, (error, results, fields) => {
-            if(error){
-                return res.status(400).json(error);
-            }
+            if(error) return res.status(400).json(error);          
             res.status(201).json("Commentaire crée.");
         })
     }catch(error){
         res.status(500).json(error);
     };
 };
-//*** Suppression d'un commentaire ***//
-//-----------------------------------//
+
+// Suppression d'un commentaire
 exports.deleteCom = (req, res, next) => {
     const find = 'SELECT * FROM comment WHERE id = ?';
     const remove = 'DELETE FROM comment WHERE id = ?';
@@ -29,15 +30,15 @@ exports.deleteCom = (req, res, next) => {
             // Controle d'erreur et d'authentification avec l'user_id
             if(error){
                 return res.status(400).json(error);
+            }else if(!results[0]){
+                return res.status(404).json("Aucun resultat.")
             }else if(results[0].user_id !== req.auth.user_id && req.auth.admin !== 1){
                 res.clearCookie('jwt');
                 return res.status(401).json('Unauthorized.');
             }
             // Suppression du commentaire
             connect.query(remove, req.body.id, (error, results, fields) => {
-                if(error){
-                    return res.status(400).json(error);
-                }
+                if(error) return res.status(400).json(error);         
                 res.status(200).json("Commentaire supprimé.");
             });
         });
@@ -45,8 +46,8 @@ exports.deleteCom = (req, res, next) => {
         res.status(500).json(error);
     };
 };
-//*** Récupération d'un commentaires ***//
-//--------------------------------------------//
+
+// Récupération d'un commentaires
 exports.getPostComs = (req, res, next) => {
     // Récupère tous les COM du POST du req.params.id
     const getAll = 'SELECT c.*, u.name, u.firstname, u.url FROM comment c INNER JOIN user u ON c.user_id = u.id WHERE c.post_id = ? ORDER BY c.date ASC';
--- back/controllers/post-controller.js
+++ back/controllers/post-controller.js
@@ -1,9 +1,7 @@
 const {connect} = require('../DB.config/db.connexion');
 const fs = require('fs');
-const { connexion } = require('./user-controller');
 
-//*** Création d'un post avec/sans image/commentaire ***//
-//-----------------------------------------------------//
+// Création d'un post avec/sans image/commentaire
 exports.createPost = (req, res, next) => {
     // Action a effectuer dans la base de donnée
     const sql = 'INSERT INTO post SET ?';
@@ -34,8 +32,7 @@ exports.createPost = (req, res, next) => {
     };
 };
 
-//*** Modification d'un POST ***//
-//-----------------------------//
+// Modification d'un POST 
 exports.modifyPost = (req, res, next) => {
     // Action a effectuer dans la base de donnée
     const find = 'SELECT posturl, userId FROM post WHERE post_id = ?';
@@ -43,7 +40,6 @@ exports.modifyPost = (req, res, next) => {
     const modifyPostData = (instruction, data) => {
         connect.query(instruction, data, (error, results, fields) => {
             if(error) return res.status(400).json(error);
-
             res.status(200).json("Modification réussi.");
         });
     }
@@ -77,6 +73,7 @@ exports.modifyPost = (req, res, next) => {
                 const sql = 'UPDATE post SET message = ? WHERE post_id = ?';
                 const options = [req.body.message, req.body.post_id];
                 modifyPostData(sql, options)
+            
             }else{
                 res.status(400).json({error: "Format de modification non conforme."})
             }
@@ -86,8 +83,7 @@ exports.modifyPost = (req, res, next) => {
     }   
 };
 
-//*** Suppression de l'image d'un post ***//
-//---------------------------------------//
+// Suppression de l'image d'un post
 exports.deletePostImage = (req, res, next) => {
     // Action a effectuer dans la base de donnée
     const find = 'SELECT posturl, userId FROM post WHERE post_id = ?';
@@ -96,9 +92,11 @@ exports.deletePostImage = (req, res, next) => {
     try{
         // Contrôle de l'existence du post dans la base de donnée
         connect.query(find, req.body.post_id, (error, results, fields) => {
-            // Contrôl d'erreur et verification de l'utilisateur
+            // Contrôl d'erreur / présence du post / verification de l'utilisateur
             if(error){
                 return res.status(400).json(error);
+            }else if(!results[0]){
+                return res.status(404).json("Aucun resultat.")
             }else if(results[0].userId !== req.auth.user_id && req.auth.admin !== 1){
                 res.clearCookie('jwt');
                 return res.status(401).json("Unauthorized.")
@@ -106,9 +104,8 @@ exports.deletePostImage = (req, res, next) => {
             // Suprression de l'image du POST s'il y en a une , puis supression du POST
             fs.unlink(`./images/post_images/${results[0].posturl}`,() => {
                 connect.query(deletePostImage, req.body.post_id, (error, results, fields) => {
-                    if(error){
-                        return res.status(400).json(error);
-                    }
+                    if(error) return res.status(400).json(error);
+                    
                     res.status(200).json("Suppression réussi.");
                 })
             });
@@ -117,8 +114,8 @@ exports.deletePostImage = (req, res, next) => {
         res.status(500).json(error);
     }
 }
-//*** Suppression d'un POST ***//
-//----------------------------//
+
+// Suppression d'un POST
 exports.deletePost = (req, res, next) => {
     // Action a effectuer dans la base de donnée
     const find = 'SELECT posturl, userId FROM post WHERE post_id = ?';
@@ -130,19 +127,20 @@ exports.deletePost = (req, res, next) => {
     try{
         // Contrôle de l'existence du post dans la base de donnée
         connect.query(find, req.body.post_id, (error, results, fields) => {
-            // Contrôl d'erreur et verification de l'utilisateur
+            // Contrôl d'erreur / présence du post / verification de l'utilisateur
             if(error){
                 return res.status(400).json(error);
+            }else if(!results[0]){
+                return res.status(404).json("Aucun resultat.")
             }else if(results[0].userId !== req.auth.user_id && req.auth.admin !== 1){
                 res.clearCookie('jwt');
                 return res.status(401).json("Unauthorized.")
             }
+            
             // Suprression de l'image du POST s'il y en a une , puis supression du POST
             fs.unlink(`./images/post_images/${results[0].posturl}`,() => {
                 connect.query(deletePost, req.body.post_id, (error, results, fields) => {
-                    if(error){
-                        return res.status(400).json(error);
-                    }
+                    if(error) return res.status(400).json(error);
                     res.status(200).json("Suppression réussi.");
                 })
             });
@@ -151,57 +149,58 @@ exports.deletePost = (req, res, next) => {
         res.status(500).json(error);
     }
 };
-//*** Like/Dislike un POST ***//
-//---------------------------//
+
+// Like/Dislike un POST 
 exports.likes = (req, res, next) => {
     // Actions a effectuer dans la base de donnée
     const find = 'SELECT * FROM likes WHERE post_id = ? AND user_id = ?';
     const remove = 'DELETE FROM likes WHERE post_id = ? AND user_id = ?';
     const insert = 'INSERT INTO likes SET post_id = ?, user_id = ?';
     const options = [req.body.post_id, req.body.user_id];
-    try{
-        // Contrôle de la présence d'un like pour ce post ou non
-        connect.query(find, options, (error, results, fields) => {
+    // Fonction réutilisable
+    const likeOrDislike = (instruction, data, message) => {
+        connect.query(instruction, data, (error, results, fields) => {
             if(error){
                 return res.status(400).json(error);
             }
+            res.status(200).json(message);
+        });
+    }
+    try{
+        // Controle des id utilisateurs
+        if(req.body.user_id !== req.auth.user_id){
+            res.clearCookie("jwt")
+            return res.status(401).json('Unauthorired')
+        }
+        // Contrôle de la présence d'un like pour ce post ou non
+        connect.query(find, options, (error, results, fields) => {
+            if(error) return res.status(400).json(error);
+            
             // S'il n'y a pas de like
             if(!results[0]){
-                connect.query(insert, options, (error, results, fields) => {
-                    if(error){
-                        return res.status(400).json(error);
-                    }
-                    res.status(200).json('Like effectué.');
-                });
-            //S'il y a un like
+                likeOrDislike(insert, options, 'Like effectué.');
             }else{
-                connect.query(remove, options, (error, results, fields) => {
-                    if(error){
-                        return res.status(400).json(error);
-                    }
-                    res.status(200).json('Dislike effectué.');
-                });
+                likeOrDislike(remove, options, 'Dislike effectué.' )
             }
         })
     }catch(error){
         res.status(500).json(error);
     }
 };
-//*** Récupération de tout les POST / Le NB de LIKE par POST / Le NB de COM par POST ***//
-//----------------------------------------------------------------------------------------------//
+
+// Récupération de tout les POST / Le NB de LIKE par POST / Le NB de COM par POST 
 exports.getAllPost = (req, res, next) => {
     // Recuperation de tout les POST avec les noms utilisateur, leurs avatar
     // On y ajoute nbLikes qui fournit le nombre de likes grace a COUNT() de SQL
     // On y ajoute isLiked qui repond par 0 ou 1 pour savoir si l'utilisateur a liké, grace a COUNT() de SQL
-    // On JOIN les infos utilisateur a leurs commentaire avec JOIN u.id = p.userId
+    // On JOIN le nombre de commentaire par post au nom de nbComs
     // On ordonne le resultat grâce a ma methode ORDER de SQL
-    const findAll = "SELECT p.*, u.name, u.firstname, u.url,\
-    (SELECT COUNT(*) FROM likes WHERE post_id = p.post_id) as nbLikes,\
-    (SELECT COUNT(*) FROM likes WHERE post_id = p.post_id AND user_id = ?) as isLiked,\
-    (SELECT COUNT(*) FROM comment WHERE post_id = p.post_id ) as nbComs FROM post p\
-    INNER JOIN user u ON u.id = p.userId\
-    ORDER BY p.post_id DESC"
-
+    const findAll = "SELECT post.*, user.name, user.firstname, user.url,\
+    (SELECT COUNT(*) FROM likes WHERE post_id = post.post_id) as nbLikes,\
+    (SELECT COUNT(*) FROM likes WHERE post_id = post.post_id AND user_id = ?) as isLiked,\
+    (SELECT COUNT(*) FROM comment WHERE post_id = post.post_id ) as nbComs FROM post \
+    INNER JOIN user ON user.id = post.userId ORDER BY post.post_id DESC"
+    
     try{
         connect.query(findAll, req.params.id, (error, results, fields) => {
             if(error){
@@ -216,12 +215,10 @@ exports.getAllPost = (req, res, next) => {
     };
 };
 
-//*** Récupération de tout les Likes d'un POST ***//
-//-----------------------------------------------//
+// Récupération de tout les Likes d'un POST 
 exports.getPostLikes = (req, res, next) => {
     // Actions a effectuer dans la base de donnée
-    const find = 'SELECT COUNT(*) as likes, \
-    (SELECT COUNT(*) FROM likes WHERE post_id = ? AND user_id = ?) as isLiked FROM likes WHERE post_id = ?';
+    const find = 'SELECT COUNT(*) as likes, (SELECT COUNT(*) FROM likes WHERE post_id = ? AND user_id = ?) as isLiked FROM likes WHERE post_id = ?';
     const options = [req.params.id, req.auth.user_id, req.params.id];
 
     try{
