69aa436d118c834feda5992c89626588c9ce288e
khersinpro <khersinpro@gmail.com>
2022-08-01 11:18:32 +0200

    last modif

--- back/DB.config/db.connexion.js
+++ back/DB.config/db.connexion.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
+require('dotenv').config()
 
 exports.connect = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'groupomania'
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DATABASE
 });
